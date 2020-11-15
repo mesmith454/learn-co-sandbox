1d835cbc2d5b69ada7cd7fe2aa08f46cfaa02457
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-15 22:58:22 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,2 @@
+# this is a good comment
+#this is an example of a bad comment: puts (1 + # 1)
---
+++ testing.rb
@@ -0,0 +1,8 @@
+name = "Brunhilde"
+viking = "Valkyrie"
+
+def greeting_warrior(name, viking)
+  puts "Velcomyr, #{name}. We heard you are a great #{viking}."
+end
+
+greeting_warrior(name, viking)
